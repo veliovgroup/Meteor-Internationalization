6ed5a3717ad7c55dd7893400483f18749b1c687f
dr.dimitru <ceo@veliov.com>
2017-11-09 18:00:31 +0300

    v3.0.3
     - Dependencies update
     - Compatibility with `meteor@1.6`

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'ostrio:i18n',
   summary: 'Super-Lightweight and fast i18n isomorphic driver for Meteor with support of placeholders.',
-  version: '3.0.2',
+  version: '3.0.3',
   git: 'https://github.com/VeliovGroup/Meteor-Internationalization',
   documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('1.4');
-  api.use(['underscore', 'check', 'reactive-var', 'ecmascript', 'ostrio:cstorage@2.2.0'], ['client', 'server']);
+  api.use(['underscore', 'check', 'reactive-var', 'ecmascript', 'ostrio:cstorage@2.2.1'], ['client', 'server']);
   api.use(['templating', 'tracker'], 'client');
   api.mainModule('i18n.js', ['client', 'server']);
 });
